5fb68930c2361c3b9496c2bf0e5f9ef337fd4b49
cnievaslozano <cnievaslozano@gmail.com>
2023-11-06 20:18:15 +0100

    Sample v2

--- resources/views/components/crypto-list.blade.php
+++ resources/views/components/crypto-list.blade.php
@@ -1,5 +1,5 @@
 <div class="container mx-auto">
-    <h1 class="text-2xl font-bold mb-4 bg-white">Tabla de Criptomonedas</h1>
+    <h1 class="text-2xl font-bold mb-4 p-3 bg-white">Crypto Currency Table</h1>
 
     <table class="min-w-full bg-white rounded-lg overflow-hidden shadow-lg">
         <thead class="bg-gray-100 text-gray-700">
@@ -9,7 +9,7 @@
                 <th class="text-left py-3 px-4 font-semibold">Símbolo</th>
                 <th class="text-left py-3 px-4 font-semibold">Precio</th>
                 <th class="text-left py-3 px-4 font-semibold">Volumen</th>
-                <th class="text-left py-3 px-4 font-semibold">Cambios (24h)</th>
+                <th class="text-left py-3 px-4 font-semibold">Market Cap</th>
             </tr>
         </thead>
         <tbody class="text-gray-600">
@@ -20,7 +20,7 @@
                 <td class="py-3 px-4">BTC</td>
                 <td class="py-3 px-4">$58,000</td>
                 <td class="py-3 px-4">$10B</td>
-                <td class="py-3 px-4 text-green-500">+5%</td>
+                <td class="py-3 px-4">$45237345234</td>
             </tr>
             <tr>
                 <td class="py-3 px-4">2</td>
@@ -28,7 +28,7 @@
                 <td class="py-3 px-4">ETH</td>
                 <td class="py-3 px-4">$3,800</td>
                 <td class="py-3 px-4">$5B</td>
-                <td class="py-3 px-4 text-green-500">+3%</td>
+                <td class="py-3 px-4">$45237345234</td>
             </tr>
             <tr>
                 <td class="py-3 px-4">3</td>
@@ -36,7 +36,7 @@
                 <td class="py-3 px-4">XRP</td>
                 <td class="py-3 px-4">$1.5</td>
                 <td class="py-3 px-4">$2B</td>
-                <td class="py-3 px-4 text-red-500">-2%</td>
+                <td class="py-3 px-4">$45237345234</td>
             </tr>
             <!-- Puedes agregar más filas para más criptomonedas -->
         </tbody>
