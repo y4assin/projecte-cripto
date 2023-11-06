ca560373e6a462ac89280e091a0124303b57376e
cnievaslozano <cnievaslozano@gmail.com>
2023-11-06 20:07:20 +0100

    Sample version

---
+++ resources/views/components/crypto-list.blade.php
@@ -0,0 +1,44 @@
+<div class="container mx-auto">
+    <h1 class="text-2xl font-bold mb-4 bg-white">Tabla de Criptomonedas</h1>
+
+    <table class="min-w-full bg-white rounded-lg overflow-hidden shadow-lg">
+        <thead class="bg-gray-100 text-gray-700">
+            <tr>
+                <th class="text-left py-3 px-4 font-semibold">#</th>
+                <th class="text-left py-3 px-4 font-semibold">Nombre</th>
+                <th class="text-left py-3 px-4 font-semibold">Símbolo</th>
+                <th class="text-left py-3 px-4 font-semibold">Precio</th>
+                <th class="text-left py-3 px-4 font-semibold">Volumen</th>
+                <th class="text-left py-3 px-4 font-semibold">Cambios (24h)</th>
+            </tr>
+        </thead>
+        <tbody class="text-gray-600">
+            <!-- Ejemplo de datos de criptomonedas -->
+            <tr>
+                <td class="py-3 px-4">1</td>
+                <td class="py-3 px-4">Bitcoin</td>
+                <td class="py-3 px-4">BTC</td>
+                <td class="py-3 px-4">$58,000</td>
+                <td class="py-3 px-4">$10B</td>
+                <td class="py-3 px-4 text-green-500">+5%</td>
+            </tr>
+            <tr>
+                <td class="py-3 px-4">2</td>
+                <td class="py-3 px-4">Ethereum</td>
+                <td class="py-3 px-4">ETH</td>
+                <td class="py-3 px-4">$3,800</td>
+                <td class="py-3 px-4">$5B</td>
+                <td class="py-3 px-4 text-green-500">+3%</td>
+            </tr>
+            <tr>
+                <td class="py-3 px-4">3</td>
+                <td class="py-3 px-4">Ripple</td>
+                <td class="py-3 px-4">XRP</td>
+                <td class="py-3 px-4">$1.5</td>
+                <td class="py-3 px-4">$2B</td>
+                <td class="py-3 px-4 text-red-500">-2%</td>
+            </tr>
+            <!-- Puedes agregar más filas para más criptomonedas -->
+        </tbody>
+    </table>
+</div>
